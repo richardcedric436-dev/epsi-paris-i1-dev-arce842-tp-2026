eb5d13293b84d5f4409d22ab9970e4c4cbd6d456
richardcedric436-dev <richardcedric436@gmail.com>
2026-03-25 13:22:31 +0000

    Add subject decorator and remove Student class

--- school.py
+++ school.py
@@ -1,76 +1,12 @@
-from collections.abc import Iterable, Iterator
+def add_subject(subject_name, default_grade=0):
+    def decorator(cls):
+        original_init = cls.__init__
 
+        def new_init(self, name, grades):
+            original_init(self, name, grades)
+            self.grades[subject_name] = default_grade
 
-class Student:
-    """Représente un étudiant avec 3 notes dans 3 matières."""
+        cls.__init__ = new_init
+        return cls
 
-    def __init__(self, name: str, grade1: float, grade2: float, grade3: float):
-        self.name = name
-        self.grade1 = grade1
-        self.grade2 = grade2
-        self.grade3 = grade3
-
-    @property
-    def average(self) -> float:
-        return (self.grade1 + self.grade2 + self.grade3) / 3
-
-    def __repr__(self):
-        return (
-            f"Student(name={self.name!r}, "
-            f"grade1={self.grade1}, grade2={self.grade2}, grade3={self.grade3}, "
-            f"average={self.average:.2f})"
-        )
-
-
-class Matter1Iterator(Iterator):
-    """Itérateur qui parcourt les étudiants du meilleur au moins bon en matière 1."""
-
-    def __init__(self, students: list):
-        self._students = sorted(students, key=lambda s: s.grade1, reverse=True)
-        self._index = 0
-
-    def __next__(self) -> Student:
-        if self._index >= len(self._students):
-            raise StopIteration
-        student = self._students[self._index]
-        self._index += 1
-        return student
-
-    def __iter__(self):
-        return self
-
-
-class Matter2Iterator(Iterator):
-    """Itérateur qui parcourt les étudiants du meilleur au moins bon en matière 2."""
-
-    def __init__(self, students: list):
-        self._students = sorted(students, key=lambda s: s.grade2, reverse=True)
-        self._index = 0
-
-    def __next__(self) -> Student:
-        if self._index >= len(self._students):
-            raise StopIteration
-        student = self._students[self._index]
-        self._index += 1
-        return student
-
-    def __iter__(self):
-        return self
-
-
-class Matter3Iterator(Iterator):
-    """Itérateur qui parcourt les étudiants du meilleur au moins bon en matière 3."""
-
-    def __init__(self, students: list):
-        self._students = sorted(students, key=lambda s: s.grade3, reverse=True)
-        self._index = 0
-
-    def __next__(self) -> Student:
-        if self._index >= len(self._students):
-            raise StopIteration
-        student = self._students[self._index]
-        self._index += 1
-        return student
-
-    def __iter__(self):
-        retu
+    return decorator
