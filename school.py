8144ecfd661c0f71b06d2094a0f86be19b2faeea
richardcedric436-dev <richardcedric436@gmail.com>
2026-03-25 13:25:29 +0000

    Update school.py

--- school.py
+++ school.py
@@ -1,4 +1,4 @@
-def add_subject(subject_name, default_grade=0):
+def add_subject_with_iterator(subject_name, default_grade=0):
     def decorator(cls):
         original_init = cls.__init__
 
@@ -6,7 +6,13 @@ def add_subject(subject_name, default_grade=0):
             original_init(self, name, grades)
             self.grades[subject_name] = default_grade
 
+        # Ajout d'une méthode pour récupérer un itérateur sur la matière
+        def get_subject_iterator(self):
+            return iter([self.grades[subject_name]])
+
         cls.__init__ = new_init
+        cls.get_subject_iterator = get_subject_iterator
+
         return cls
 
     return decorator
